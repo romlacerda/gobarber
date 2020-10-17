7badd3a336b287097b739403cc9dab8914a075c0
Romulo Lacerda <rlcrweb@gmail.com>
2020-10-17 20:55:11 -0300

    feat: recover and forgot password routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,16 @@ import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
 import Route from './Route';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" component={SignIn} exact />
       <Route path="/signup" component={SignUp} />
+      <Route path="/forgot-password" component={ForgotPassword} />
+      <Route path="/reset-password" component={ResetPassword} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
